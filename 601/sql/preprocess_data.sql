8538e27a68406aaa47c327eeb5450d82268c3119
huangjiajun <2023200808@mail.buct.edu.cn>
2024-10-31 18:48:31 +0800

    delete sql

--- 601/sql/preprocess_data.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO `preprocessed_dataset` VALUES (1,'hjj_pre_data1','hjj_pre_data1','/',9,
-                                           '2015-08-25 10:34:54', 'admin',1);
-INSERT INTO `preprocessed_dataset` VALUES (2,'hjj_pre_data2','hjj_pre_data2','/csv2',5,
-                                           '2015-08-26 10:34:54', 'admin',2);
